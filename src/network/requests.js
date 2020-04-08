b121274f267392dc36f8fac14b786f16096d7c3b
Charlie <charlie.chen@chinapcbone.com.cn>
2020-04-08 18:46:52 +0800

    update

--- src/network/requests.js
+++ src/network/requests.js
@@ -1,7 +1,7 @@
 import originAxios from 'axios'
 export default function axios(config) {
   const instance = originAxios.create({
-    baseURL: 'http://123.207.32.32:8000/api/h8',
+    baseURL: '',
     timeout: 5000,
     headers: '',
   });
